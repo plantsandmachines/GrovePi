9fdb35f9f34914a483c892ca247f414841622b85
Frank Urbainsky <frank@plantsandmachines.de>
2019-10-21 20:00:32 +0200

    changes to encoder

--- Software/NodeJS/libs/sensors/encoderDigitalSensor.js
+++ Software/NodeJS/libs/sensors/encoderDigitalSensor.js
@@ -7,37 +7,48 @@ function EncoderDigitalSensor(pin) {
 }
 EncoderDigitalSensor.prototype = new DigitalSensor()
 
+var lastSignal = 1;
+
 EncoderDigitalSensor.prototype.read = function() {
-  var write = this.board.writeBytes(commands.encoderRead.concat([this.pin, commands.unused, commands.unused]))
+  var write = this.board.writeBytes(commands.dRead.concat([this.pin, commands.unused, commands.unused]))
   if (write) {
-    this.board.wait(200)
+    this.board.wait(100)
     this.board.readByte()
     var bytes = this.board.readBytes()
-    if (bytes instanceof Buffer && bytes[1] != 255)
-      return [bytes[1], bytes[2]]
-    else
+
+    //console.log('Encoder test' ,bytes instanceof Buffer, bytes[0],bytes[1],lastSignal != bytes[0]);
+    if (lastSignal != bytes[0]){
+      lastSignal = bytes[0];
+      return true;
+    }
+    else {
       return false
+    }
   } else {
+    console.log( ' encoder write of "read" command failed');
     return false
   }
 }
 EncoderDigitalSensor.prototype.enable = function() {
   var write = this.board.writeBytes(commands.encoderEn.concat([commands.unused, commands.unused, commands.unused]))
   if (write) {
-    this.board.wait(200)
+    console.log('wrote enable of encoder')
+    this.board.wait(200);
     return true
   } else {
+    console.log('could not write enable of encoder')
     return false
   }
 }
 EncoderDigitalSensor.prototype.disable = function() {
   var write = this.board.writeBytes(commands.encoderDis.concat([commands.unused, commands.unused, commands.unused]))
   if (write) {
-    this.board.wait(200)
+    this.board.wait(200);
     return true
   } else {
     return false
   }
 }
+
 
 module.exports = EncoderDigitalSensor
